e2f87536a4ef8ca6283f0a4000a6c55bfca37dfd
David Montesdeoca <backpackerhh@gmail.com>
2021-01-25 19:49:58 +0100

    Allow to navigate within app

--- client/index.js
+++ client/index.js
@@ -2,14 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
+import { Router, Switch, Route } from "react-router-dom";
+import { createHashHistory } from "history";
+
 import SongList from "./components/SongList";
 
 const client = new ApolloClient({});
+const history = createHashHistory();
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router history={history}>
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={SongList} />
+          </Switch>
+        </div>
+      </Router>
     </ApolloProvider>
   );
 };
