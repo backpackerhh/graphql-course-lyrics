fa772e7209702b08d796cdb18b00d4f58b135d6a
David Montesdeoca <backpackerhh@gmail.com>
2021-01-25 18:57:33 +0100

    Allow to display some info about songs from MongoDB

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,7 +1,30 @@
 import React from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
-const SongList = () => {
-  return <div>SongList</div>;
+const SongList = ({ data }) => {
+  const { songs, loading } = data;
+
+  const renderSongs = () => {
+    return songs.map((song) => {
+      return <li key={song.id}>{song.title}</li>;
+    });
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return <ul>{renderSongs()}</ul>;
 };
 
-export default SongList;
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
