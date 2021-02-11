1489d2ad478501b2d6583214440ee6d26e261584
David Montesdeoca <backpackerhh@gmail.com>
2021-02-11 21:50:39 +0100

    Allow to like lyrics of a song

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { useMutation } from "@apollo/client";
+
+import likeLyricMutation from "../mutations/likeLyric";
 
 const LyricList = ({ lyrics }) => {
+  const [likeLyric, { data }] = useMutation(likeLyricMutation);
+
+  const handleLike = (id) => {
+    likeLyric({ variables: { id } });
+  };
+
   const renderLyrics = () => {
     return lyrics.map((lyric) => {
       return (
         <li key={lyric.id} className="collection-item">
           {lyric.content}
+
+          <div className="vote-box">
+            <i className="material-icons" onClick={() => handleLike(lyric.id)}>
+              thumb_up
+            </i>
+
+            {lyric.likes}
+          </div>
         </li>
       );
     });
--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { gql, useMutation } from "@apollo/client";
+import { useMutation } from "@apollo/client";
 import { Link, useHistory } from "react-router-dom";
 
 import GoBack from "./GoBack";
